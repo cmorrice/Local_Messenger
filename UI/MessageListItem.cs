73d541c45fe90c246e5cf0906ba2f732fa5d2ae2
Camilo Morrice <cmorrice2000@gmail.com>
2022-11-13 22:22:25 -0500

    Working on Message List themes

--- UI/MessageListItem.cs
+++ UI/MessageListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,50 @@ using System.Windows.Controls.Primitives;
 
 namespace Local_Messenger
 {
-    class MessageListItem : TextBlock
+    class MessageListItem : Button
     {
+        public enum Themes
+        {
+            SenderTop, SenderMiddle, SenderBottom, SenderSolo,
+            ReceiverTop, ReceiverMiddle, ReceiverBottom, ReceiverSolo
+        }
+
+        static private Style SenderTopTheme { get; }
+        static private Style SenderMiddleTheme { get; }
+        static private Style SenderBottomTheme { get; }
+        static private Style SenderSoloTheme { get; }
+        static private Style ReceiverTopTheme { get; }
+        static private Style ReceiverMiddleTheme { get; }
+        static private Style ReceiverBottomTheme { get; }
+        static private Style ReceiverSoloTheme { get; }
+
+        static private Style[] themes = new Style[8];
+
+
         static private Style buttonTheme { get; }
         static MessageListItem()
         {
-            // creates the toggle button style
-            buttonTheme = new Style(typeof(RadioButton), new Style(typeof(ToggleButton)));
-            ControlTemplate buttonTemplate = new ControlTemplate(typeof(RadioButton));
-            FrameworkElementFactory elementFactory = new FrameworkElementFactory(typeof(ContentPresenter));
-            buttonTemplate.VisualTree = elementFactory;
-            buttonTheme.Setters.Add(new Setter { Property = Button.TemplateProperty, Value = buttonTemplate });
+            // create each of the styles
+            for (int index = 0; index < 8; index++)
+            {
+                themes[index] = new Style(typeof(Button));
+                ControlTemplate template = new ControlTemplate(typeof(Button));
+
+                FrameworkElementFactory factory = new FrameworkElementFactory(typeof(Border));
+                factory.SetValue(Border.BackgroundProperty, Brushes.Green);
+                factory.SetValue(Border.CornerRadiusProperty, new CornerRadius(5, 10, 10, 5));
+                factory.SetValue(HeightProperty, 30);
+
+                template.VisualTree = factory;
+
+                factory.AppendChild(new FrameworkElementFactory(typeof(TextBlock)));
+                factory.FirstChild.SetValue(TextBlock.TextProperty, "Message");
+                factory.FirstChild.SetValue(TextBlock.VerticalAlignmentProperty, VerticalAlignment.Center);
+                factory.FirstChild.SetValue(TextBlock.HorizontalAlignmentProperty, HorizontalAlignment.Center);
+                factory.FirstChild.SetValue(TextBlock.PaddingProperty, new Thickness(5));
+
+                themes[index].Setters.Add(new Setter { Property = Button.TemplateProperty, Value = template });
+            }
         }
 
 
