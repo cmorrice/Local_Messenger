3f7fead188d10e220e776224355d6af1c3846b18
Camilo Morrice <cmo325@uky.edu>
2022-11-17 15:51:27 -0500

    Started testing the network
    Wow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -62,13 +62,18 @@ namespace Local_Messenger
 
         public async Task startServer()
         {
-            await Task.Run(() =>
+            TcpListener server = new TcpListener(IPAddress.Any, 18604);
+            // we set our IP address as server's address, and we also set the port: 18604
+
+            server.Start();  // this will start the server
+
+            while (true)   //we wait for a connection
             {
-                TcpListener server = new TcpListener(IPAddress.Any, 18604);
-                // we set our IP address as server's address, and we also set the port: 18604
-
-                server.Start();  // this will start the server
-
+                TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
+                this.Dispatcher.Invoke(() =>
+                {
+                    Server_Out.Text = string.Format("Client Connected: {0}", ((IPEndPoint)client.Client.RemoteEndPoint).Address);
+                });
                 while (true)   //we wait for a connection
                 {
                     TcpClient client = server.AcceptTcpClient();  //if a connection exists, the server will accept it
@@ -77,18 +82,40 @@ namespace Local_Messenger
                         Chat_Box.Text = string.Format("Client Connected: {0}", ((IPEndPoint)client.Client.RemoteEndPoint).Address);
                     });
 
-                    NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
+                NetworkStream ns = client.GetStream(); //networkstream is used to send/receive messages
 
-                    byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
-                    hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
+                byte[] hello = new byte[100];   //any message must be serialized (converted to byte array)
+                hello = Encoding.Default.GetBytes("hello world");  //conversion string => byte array
 
-                    ns.Write(hello, 0, hello.Length);     //sending the message
+                ns.Write(hello, 0, hello.Length);     //sending the message
 
-                    while (client.Connected)  //while the client is connected, we look for incoming messages
-                    {
-                        byte[] msg = new byte[1024];     //the messages arrive as byte array
-                        ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
+                while (client.Connected)  //while the client is connected, we look for incoming messages
+                {
+                    byte[] msg = new byte[1024];     //the messages arrive as byte array
+                    ns.Read(msg, 0, msg.Length);   //the same networkstream reads the message sent by the client
 
+                    this.Dispatcher.Invoke(() => { Server_Out.Text = Encoding.Default.GetString(msg); });
+                    Console.WriteLine(Encoding.Default.GetString(msg)); //now , we write the message as string
+                }
+            }
+        }
+
+        public async Task startClient()
+        {
+            await Task.Delay(1000);
+            TcpClient client = new TcpClient("127.0.0.1", 18604);
+            NetworkStream stream = client.GetStream();
+            byte[] hello = Encoding.Default.GetBytes("freaky friday");
+
+            stream.Write(hello, 0, hello.Length);
+
+            while (client.Connected)
+            {
+                byte[] msg = new byte[1024];
+                stream.Read(msg, 0, msg.Length);
+
+                this.Dispatcher.Invoke(() => { Client_Out.Text = Encoding.Default.GetString(msg); });
+            }
                         this.Dispatcher.Invoke(() => { Chat_Box.Text = Encoding.Default.GetString(msg); });
                         Console.WriteLine(Encoding.Default.GetString(msg)); //now , we write the message as string
                     }
---
+++ Network Classes/NetworkInterface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_Messenger
+{
+    public enum MessageType
+    {
+        disconnect_e,   // when the other disconnects
+        connect_e,      // a connect request
+        acknowledge_e,  // an acknowledgement
+        hash_e,         // a hash request
+        upload_e,       // an upload request
+        download_e,     // a download request
+        delete_e,       // a delete request
+        exit_e,         // an exit
+    }
+
+    public struct NetworkHeader
+    {
+        public MessageType type;          // uint32_t is typically enum
+        public Int64 payloadSize;              // int64_t is typically ssize_t
+        public string fileName;   //
+    }
+
+    class NetworkInterface
+    {
+        public NetworkInterface()
+        {
+            
+        }
+    }
+}
